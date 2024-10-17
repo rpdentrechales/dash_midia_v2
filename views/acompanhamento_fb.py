ed14f057efe83a2de18695a49c21235329878764
rpdentrechales <rpdprocorpo@gmail.com>
2024-10-17 14:16:46 +0000

    Updating files

--- views/acompanhamento_fb.py
+++ views/acompanhamento_fb.py
@@ -72,9 +72,6 @@ df_metas_unidade = load_aux_dataframe("aux - Configurar metas unidade",["unidade
 df_metas_unidade['month'] = pd.to_datetime(df_metas_unidade['month'])
 df_metas_unidade['month'] = df_metas_unidade['month'].dt.to_period('M')
 
-df_metas_categoria['month'] = pd.to_datetime(df_metas_categoria['month'])
-df_metas_categoria['month'] = df_metas_categoria['month'].dt.to_period('M')
-
 df_fb = pd.merge(df_fb,df_categorias,how="left",left_on="Ad Name",right_on="Anuncio")
 df_fb = df_fb.drop(columns=["Anuncio"])
 df_fb["Results"] = df_fb["Results"].fillna(0)
