b39794899abd4134108e244f5010deca6f8bc3c3
rpdentrechales <rpdprocorpo@gmail.com>
2024-10-17 15:26:01 +0000

    Updating files

--- views/testes.py
+++ views/testes.py
@@ -9,24 +9,31 @@ st.set_page_config(page_title="Testes", page_icon="💎",layout="wide")
 
 st.write("teste")
 
-# Sample dataframe with columns A and B, where column C is dependent on A and B
-data = {
-    'A': [10, 20, 30],
-    'B': [1, 2, 3],
-    'C': [10, 40, 90]  # Initially calculated as A * B
-}
-
-# Create a DataFrame
-df = pd.DataFrame(data)
-
-# Use st.data_editor to allow editing columns A and B
-edited_df = st.data_editor(df[['A', 'B']], num_rows="dynamic", key="unique_editor")
-
-# Recalculate column C based on the updated values in columns A and B
-edited_df['C'] = edited_df['A'] * edited_df['B']
-
-# Update the original dataframe with the recalculated values
-df.update(edited_df)
-
-# Display the updated dataframe in the same data editor
-st.data_editor(df, num_rows="dynamic", key="updated_editor")
+df = pd.DataFrame({
+     "solution": [ ],
+     "total_amount": [],
+     "service_amount": [],
+     "hours_amount": [],
+     "num_horas_S": [],
+     "num_horas_DS": [],
+ })
+ 
+def calc_hours(input_df):
+  output_df = input_df.copy()
+  output_df["num_horas"] = input_df["num_horas_S"] + input_df["num_horas_DS"]
+  
+  return output_df
+ 
+st.title("Ejemplo")
+ 
+ # Crear el editor
+editable_df_new = st.data_editor(df,
+                                   key="edit",
+                                   hide_index=True,
+                                   num_rows="dynamic",
+                                   disabled=['num_horas']
+                                   )
+ 
+if st.button("Actualizar DataFrame"):
+  new = calc_hours(editable_df_new)
+  st.write(new)
