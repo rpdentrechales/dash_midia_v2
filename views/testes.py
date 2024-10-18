5816d25cea3b8dfec4b9354bfa844acb3bd0884d
rpdentrechales <rpdprocorpo@gmail.com>
2024-10-18 13:32:28 +0000

    Updating files

--- views/testes.py
+++ views/testes.py
@@ -9,31 +9,10 @@ st.set_page_config(page_title="Testes", page_icon="💎",layout="wide")
 
 st.write("teste")
 
-df = pd.DataFrame({
-     "solution": [ ],
-     "total_amount": [],
-     "service_amount": [],
-     "hours_amount": [],
-     "num_horas_S": [],
-     "num_horas_DS": [],
- })
- 
-def calc_hours(input_df):
-  output_df = input_df.copy()
-  output_df["num_horas"] = input_df["num_horas_S"] + input_df["num_horas_DS"]
-  
-  return output_df
- 
-st.title("Ejemplo")
- 
- # Crear el editor
-editable_df_new = st.data_editor(df,
-                                   key="edit",
-                                   hide_index=True,
-                                   num_rows="dynamic",
-                                   disabled=['num_horas']
-                                   )
- 
-if st.button("Actualizar DataFrame"):
-  new = calc_hours(editable_df_new)
-  st.write(new)
+st.text_input('Name', key='name')
+
+def set_name(name):
+    st.session_state.name = name
+
+st.button('Clear name', on_click=set_name, args=[''])
+st.button('Streamlit!', on_click=set_name, args=['Streamlit'])
