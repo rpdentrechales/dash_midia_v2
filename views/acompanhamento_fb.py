b7cf7b7698277bb917f5b9860991670a71f4acc6
rpdentrechales <rpdprocorpo@gmail.com>
2024-10-18 13:20:18 +0000

    Updating files

--- views/acompanhamento_fb.py
+++ views/acompanhamento_fb.py
@@ -244,4 +244,5 @@ meta_updated = tabela_acompanhamento[["categoria","meta"]]
 meta_updated["unidade"] = store_filter
 meta_updated["plataforma"] = "Facebook"
 
-st.write(meta_updated)
+df_categorias_updated = pd.concat(df_categorias,meta_updated)
+st.write(df_categorias_updated)
