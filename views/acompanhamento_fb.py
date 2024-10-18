c7bc0f04027e397172506e49153b90f327e9ae47
rpdentrechales <rpdprocorpo@gmail.com>
2024-10-18 13:52:24 +0000

    Updating files

--- views/acompanhamento_fb.py
+++ views/acompanhamento_fb.py
@@ -242,7 +242,7 @@ tabela_acompanhamento = st.data_editor(
     }
   )
 
-porcentagem_total = categoria_groupby["meta"].sum()
+porcentagem_total = tabela_acompanhamento["meta"].sum()
 porcentagem_total = f"{porcentagem_total :.2f} %"
 
 st.write(porcentagem_total)
